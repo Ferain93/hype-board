27a89686a3480b949face10cd35e39c32dc1f764
Fernanda Trujillo <77870297+Ferain93@users.noreply.github.com>
2026-04-24 07:23:24 -0500

    fix: name url vercel (#7)

--- hype-backend/src/main.ts
+++ hype-backend/src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
     origin: [
       'http://localhost:5173',
       'http://localhost:3000',
-      'https://hype-board.vercel.app',
+      'https://hype-board-sigma.vercel.app',
     ],
     methods: ['GET'],
   });
